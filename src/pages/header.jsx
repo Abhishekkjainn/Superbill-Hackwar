c08dc26858e610c5f93ba7043142da6382b06565
abhishekkjainn <jainabhishek1904@gmail.com>
2025-01-18 01:17:21 +0530

    Header Responsive

--- src/pages/header.jsx
+++ src/pages/header.jsx
@@ -32,17 +32,20 @@ export default function Header() {
         </div>
       </div>
       <div className="actions">
-        <div className="primarybutton">
-          <img src="/add.png" alt="Add An Item" className="buttonicon" />
-          <div className="primarybuttontag">Add Item</div>
-        </div>
-        <div className="primarybutton">
-          <img src="/bill.png" alt="Add An Item" className="buttonicon" />
-          <div className="primarybuttontag">Generate Bill</div>
+        <div className="buttons">
+          <div className="primarybutton">
+            <img src="/add.png" alt="Add An Item" className="buttonicon" />
+            <div className="primarybuttontag">Add Item</div>
+          </div>
+          <div className="primarybutton">
+            <img src="/bill.png" alt="Add An Item" className="buttonicon" />
+            <div className="primarybuttontag">Generate Bill</div>
+          </div>
         </div>
         <div className="profile">
           <img src="/accountlogo.png" alt="Account" className="profileimg" />
         </div>
+        <img src="/menu.png" alt="Menu Icon" className="menuicon" />
       </div>
     </div>
   );
