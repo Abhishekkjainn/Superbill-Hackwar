1a7cc4ee920e1a545d86d751b1df37ae68bbaac7
abhishekkjainn <jainabhishek1904@gmail.com>
2025-01-18 12:21:53 +0530

    Added company Information

--- src/pages/page1.jsx
+++ src/pages/page1.jsx
@@ -1,10 +1,61 @@
+import { useState } from 'react';
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
+
 export default function Page1() {
+  const options = [
+    'Today',
+    'This Week',
+    'This Month',
+    'Last 3 Months',
+    'Last 6 Months',
+    'Last Year',
+    'All Time',
+  ];
+  const defaultOption = options[0];
+  const [selectedOption, setSelectedOption] = useState(defaultOption);
+
+  const handleSelect = (option) => {
+    setSelectedOption(option.value);
+    console.log('Selected:', option.value); // For debugging
+  };
+
   var totalitems = 150;
   var totalSoldtoday = 60;
   var GST = '11,600';
+  var analyticslogo =
+    'https://cdn.brandfetch.io/zudio.com/fallback/lettermark/theme/dark/h/256/w/256/icon?c=1bfwsmEH20zzEfSNTed';
+  var analyticscompany = 'Zudio - Bhilai TI Mall';
+  var ownername = 'Abhishek Jain';
+  var imagelink =
+    'https://preview.redd.it/new-profile-pictures-for-yall-v0-brdjms2xte3c1.jpg?width=640&crop=smart&auto=webp&s=a785e9ec16789276c74b19398f4f41b471f672f8';
+
   return (
     <div className="page1">
-      <div className="filter"></div>
+      <div className="companyinfo">
+        <div className="companyinformation">
+          <img
+            src={analyticslogo}
+            alt="Analytics Logo"
+            className="analyticslogo"
+          />
+          <div className="companynames">
+            <div className="analyticscompany">{analyticscompany}</div>
+            <div className="ownertag">
+              <img src={imagelink} alt="Owner Image" className="ownerimg" />
+              <div className="ownername">{ownername}</div>
+            </div>
+          </div>
+        </div>
+
+        <Dropdown
+          options={options}
+          onChange={handleSelect}
+          value={selectedOption}
+          placeholder="Select an option"
+          className="filters"
+        />
+      </div>
       <div className="analytics">
         <div className="analytic inventoryvalue">
           <div className="top">
